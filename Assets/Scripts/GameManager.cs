1a785e7a2947aacd25a5f978238a7ebe7ac1651f
unknown <you@example.com>
2022-07-24 00:54:17 +0700

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    public GameObject[] obstaclePbs;
+    public GameObject player;
+    public BGLoop bgLoop;
+    public bool isGameover;
+    public bool isGameplaying;
+    private int m_score;
+
+    public int Score
+    {
+        get
+        {
+            return m_score;
+        }
+
+        set
+        {
+            m_score = value;
+        }
+    }
+
+    public override void Awake()
+    {
+        MakeSingleton(false);
+    }
+
+
+    public override void Start()
+    {
+        base.Start();
+
+        StartCoroutine(SpawnObstacle());
+    }
+
+
+    IEnumerator SpawnObstacle()
+    {
+        while (!isGameover)
+        {
+            float spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+
+            yield return new WaitForSeconds(spawnTime);
+
+            if(obstaclePbs != null && obstaclePbs.Length > 0)
+            {
+                int obIdx = Random.Range(0, obstaclePbs.Length);
+
+                GameObject obstacle = obstaclePbs[obIdx];
+
+                if (obstacle)
+                {
+                    Vector3 spawnPos = new Vector3 (Random.Range(-1.4f, 1.4f), 8f, 0f);
+                    Instantiate(obstacle, spawnPos, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GamepadController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamepadController : Singleton<GamepadController> {
+
+    public bool isOnMobile;
+    bool m_canMoveLeft;
+    bool m_canMoveRight;
+
+    public bool CanMoveLeft
+    {
+        get
+        {
+            return m_canMoveLeft;
+        }
+
+        set
+        {
+            m_canMoveLeft = value;
+        }
+    }
+
+    public bool CanMoveRight
+    {
+        get
+        {
+            return m_canMoveRight;
+        }
+
+        set
+        {
+            m_canMoveRight = value;
+        }
+    }
+
+    public override void Awake()
+    {
+        MakeSingleton(false);
+    }
+
+    void PCInputHandle()
+    {
+        if (isOnMobile) return;
+
+        m_canMoveLeft = Input.GetAxisRaw("Horizontal") < 0 ? true : false;
+
+        m_canMoveRight = Input.GetAxisRaw("Horizontal") > 0 ? true : false;
+    }
+
+    private void Update()
+    {
+        PCInputHandle();
+    }
+}
